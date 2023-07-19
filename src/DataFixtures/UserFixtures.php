6b99a92216c103e47fba50dcce36f4fac96c54db
ArnaudBvd <bravard.arnaud@gmail.com>
2023-07-19 20:57:51 +0200

    création de 2 utilisateurs test (user & admin) avec des fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,15 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private $hasher;
+ 
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $testUser = new User();
+        $testUser->setLastname("user");
+        $testUser->setFirstname("user");
+        $testUser->setEmail("user@user.fr");
+        $encodedPassword = $this->hasher->hashPassword($testUser, "user");
+        $testUser->setPassword($encodedPassword);
+
+        $testAdmin = new User();
+        $testAdmin->setLastname("admin");
+        $testAdmin->setFirstname("admin");
+        $testAdmin->setEmail("admin@admin.fr");
+        $encodedPassword = $this->hasher->hashPassword($testUser, "admin");
+        $testAdmin->setPassword($encodedPassword);
+        $testAdmin->setRoles(["ROLE_ADMIN"]);
+
+        $manager->persist($testUser);
+        $manager->persist($testAdmin);
 
         $manager->flush();
     }
