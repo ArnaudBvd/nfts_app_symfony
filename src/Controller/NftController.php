e8987374b0b58ae26c7a7dd4fe57bddf16fd2b0e
Arnaud Bravard <bravard.arnaud@gmail.com>
2023-07-24 13:53:09 +0200

    Sécurisation des routes avec le isGranted

--- src/Controller/NftController.php
+++ src/Controller/NftController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class NftController extends AbstractController
 {
@@ -17,6 +18,7 @@ class NftController extends AbstractController
     }
 
     #[Route('/nft/account', name: 'app_nft')]
+    #[IsGranted('ROLE_USER')]
     public function nftUserId(): Response
     {
         return $this->render('nft/index.html.twig', [
