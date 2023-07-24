704abdf2a6ae18ba1bb1027fbb0b82a01cbb4037
ArnaudBvd <bravard.arnaud@gmail.com>
2023-07-24 22:39:32 +0200

    Création d'un formulaire d'ajout de NFT + modification du NftType pour le upload de fichier

--- src/Controller/NftController.php
+++ src/Controller/NftController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Nft;
+use App\Form\NftType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,12 +19,23 @@ class NftController extends AbstractController
         ]);
     }
 
-    #[Route('/nft/account', name: 'app_nft')]
+    #[Route('/nft/account', name: 'app_nft_user')]
     #[IsGranted('ROLE_USER')]
     public function nftUserId(): Response
     {
         return $this->render('nft/index.html.twig', [
             'controller_name' => 'NftController',
+        ]);
+    }
+
+    #[Route('/nft/add', name: 'app_nft_form')]
+    #[IsGranted('ROLE_USER')]
+    public function nftAjout(): Response
+    {
+        $form = $this->createForm(NftType::class, new Nft());
+
+        return $this->render('nft/form.html.twig', [
+            "form" => $form->createView()
         ]);
     }
 }
